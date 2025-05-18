a3f91c35d1a2843a9635a920ca4b9b5b5ba1a351
pranaykumarsamala <samalapranay02@gmail.com>
2025-05-18 22:04:03 +0530

    template-driven-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  @ViewChild('iplForm') form!: NgForm;
+    formDetails(){
+      console.log(this.form);
+  }
 }
